d39909f4a3c4046a3a8e332ec2613b36651948b3
Jaypee28 <Jaypee28@tonberry.github.com>
2020-08-27 16:58:04 +0800

    swipe server

---
+++ routes/api/profile.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const request = require('request')
+const config = require('config')
+const router = express.Router()
+const auth = require('../../middleware/auth')
+const { check, validationResult } = require('express-validator')
+
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
+
+
+// @route   GET api/profile/me
+// @des     Get current users profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+    try{
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar'])
+
+        if(!profile){
+            return res.status(400).json({ msg: 'There is no profile for this user' })
+        }
+
+        res.json(profile)
+    }catch(err){
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route   POST api/profile
+// @des     Create/Update a user profile
+// @access  Private
+router.post('/', auth, async (req, res) => {
+
+    const {
+        dateofbirth,
+        location,
+        avatar
+    } = req.body
+
+    const profileFields = {}
+    profileFields.user = req.user.id
+    if(dateofbirth) profileFields.dateofbirth = dateofbirth
+    if(location) profileFields.location = location
+    if(avatar) profileFields.avatar = avatar
+
+    try {
+        
+        let profile = await Profile.findOne({ user: req.user.id })
+
+        if(profile){
+            //Update
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true }
+            )
+
+            return res.json(profile)
+        }
+        
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route   POST api/profile/user/:user_id
+// @des     Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', [ 'name', 'avatar' ])
+
+        if(!profile) return res.status(400).json({ msg: 'Profile not found' })
+
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message)
+        if(err.kind == 'ObjectId'){
+            return res.status(400).json({ msg: 'Profile not found' })
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
+module.exports = router
