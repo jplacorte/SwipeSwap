194113cb0ac13f0d825e040471340381182cd360
Jaypee28 <Jaypee28@me.github.com>
2020-09-15 14:46:29 +0800

    removed bcryptjs

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const bcrypt = require('bcryptjs')
 const auth = require('../../middleware/auth')
 const jwt = require('jsonwebtoken')
 const config = require('config')
